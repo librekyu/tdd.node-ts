86685cf6ccc1f0cb74b3e5f3eb640fe2e9c1f701
librekyu <khkhong1@gmail.com>
2021-03-25 20:22:36 +0900

    add tdd class 5,6,7,8

--- src/spec/util.spec.ts
+++ src/spec/util.spec.ts
@@ -12,35 +12,34 @@
  *
  * */
 
-class Dollar {
-  private amount = 1;
+class Money {
+  protected amount = 1;
   constructor(amount?: number) {
     this.amount = amount || this.amount;
   }
-  public getAmount(): number {
+  static dollar(amount: number): Dollar {
+    return new Dollar(amount);
+  }
+  static franc(amount: number): Franc {
+    return new Franc(amount);
+  }
+  getAmount(): number {
     return this.amount;
   }
-  public multiply(multiplier: number): void {
+  multiply(multiplier: number): void {
     this.amount *= multiplier;
-  }
-  public equals(dollar: Dollar): boolean {
-    return this.amount === dollar.amount;
   }
 }
 
-class Franc {
-  private amount = 1;
-  constructor(amount?: number) {
-    this.amount = amount || this.amount;
+class Dollar extends Money {
+  public equals(dollar: Money): boolean {
+    return this.amount === dollar.getAmount() && typeof this === typeof dollar;
   }
-  public getAmount(): number {
-    return this.amount;
-  }
-  public multiply(multiplier: number): void {
-    this.amount *= multiplier;
-  }
+}
+
+class Franc extends Money {
   public equals(franc: Franc): boolean {
-    return this.amount === franc.amount;
+    return this.amount === franc.amount && typeof this === typeof franc;
   }
 }
 
@@ -65,12 +64,34 @@ describe('dollar test', () => {
     it('should return 10', () => {
       const dollar = new Dollar(5);
       dollar.multiply(2);
-      expect(dollar.getAmount()).toEqual(10);
+      expect(dollar.getAmount()).toBe(10);
     });
   });
-  context('', () => {
-    it('', () => {
-      const dollar = new Dollar(5);
+  context("when execute franc(5)'s multiply(2) method", () => {
+    it('should return 10', () => {
+      const franc = new Franc(5);
+      franc.multiply(2);
+      expect(franc.getAmount()).toBe(10);
+    });
+  });
+  context('when execute dollar(5) compare with dollar(5)', () => {
+    it('should return true', () => {
+      expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
+    });
+  });
+  context('when execute dollar(5) compare with franc(5)', () => {
+    it('should return true', () => {
+      expect(Money.dollar(5).equals(Money.franc(5))).toBe(true);
+    });
+  });
+  context('when execute franc(5) compare with dollar(6)', () => {
+    it('should return false', () => {
+      expect(Money.franc(5).equals(Money.dollar(6))).toBe(false);
+    });
+  });
+  context('when execute franc(6) compare with dollar(5)', () => {
+    it('should return false', () => {
+      expect(Money.franc(6).equals(Money.dollar(5))).toBe(false);
     });
   });
 });
